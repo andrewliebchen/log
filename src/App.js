6bd7437db785921fd56fc18e9fb0e2b7c17ab158
Andrew Liebchen <liebchen@fb.com>
2020-11-25 09:33:13 -0800

    Screen button and status labels

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { Flex, Text } from "theme-ui";
 import { useReactMediaRecorder } from "react-media-recorder";
+
+const statuses = {
+  media_aborted: { label: "Media aborted" },
+  permission_denied: { label: "Permission denied" },
+  no_specified_media_found: { label: "No specified media found" },
+  media_in_use: { label: "Media in use" },
+  invalid_media_constraints: { label: "Invalid media constraints" },
+  no_constraints: { label: "No constraints" },
+  recorder_error: { label: "Recorder error" },
+  idle: { label: "Whats on your mind?" },
+  acquiring_media: { label: "Acquiring media" },
+  recording: { label: "Listening" },
+  stopping: { label: "Saving" },
+  stopped: { label: "Saved" },
+};
 
 const App = () => {
   const {
@@ -9,12 +25,18 @@ const App = () => {
   } = useReactMediaRecorder({ audio: true });
 
   return (
-    <div>
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <audio src={mediaBlobUrl} controls />
-    </div>
+    <Flex
+      onMouseDown={startRecording}
+      onMouseUp={stopRecording}
+      sx={{
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Text>{statuses[status].label}</Text>
+    </Flex>
   );
 };
 
